0af5333836afe6c00ef21d213bad0e5608bd6107
luli28 <valmar1968@hotmail.com>
2022-08-11 15:07:39 -0300

    Creación components

--- proyectofinal_mrg/src/app/app.module.ts
+++ proyectofinal_mrg/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoapComponent } from './components/logoap/logoap.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HysComponent } from './components/hys/hys.component';
+import { ProyectosComponent } from './components/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoapComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    FooterComponent,
+    HysComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule,
